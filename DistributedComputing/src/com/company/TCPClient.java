04dc0476f3233f2d909986d6bdea75456b49489b
Aman Bhimani <amanb014@gmail.com>
2015-10-15 15:14:12 -0400

    Added file.txt and also Client has a change of saying "Hi" in the begining.

--- DistributedComputing/src/com/company/TCPClient.java
+++ DistributedComputing/src/com/company/TCPClient.java
@@ -12,7 +12,7 @@ public class TCPClient {
         BufferedReader in = null; // for reading form ServerRouter
         InetAddress addr = InetAddress.getLocalHost();
         String host = addr.getHostAddress(); // Client machine's IP
-        String routerName = "localhost"; // ServerRouter host name
+        String routerName = "172.20.10.6"; // ServerRouter host name
         int SockNum = 5555; // port number
 
         // Tries to connect to the ServerRouter
@@ -35,7 +35,7 @@ public class TCPClient {
         BufferedReader fromFile =  new BufferedReader(reader); // reader for the string file
         String fromServer; // messages received from ServerRouter
         String fromUser; // messages sent to ServerRouter
-        String address ="10.5.2.109"; // destination IP (Server)
+        String address ="172.20.10.3"; // destination IP (Server)
         long t0, t1, t;
 
         // Communication process (initial sends/receives
@@ -46,6 +46,7 @@ public class TCPClient {
         t0 = System.currentTimeMillis();
 
         // Communication while loop
+        out.println("Hi.");
         while ((fromServer = in.readLine()) != null) {
             System.out.println("Server: " + fromServer);
             t1 = System.currentTimeMillis();
@@ -61,7 +62,6 @@ public class TCPClient {
                 t0 = System.currentTimeMillis();
             }
         }
-
         // closing connections
         out.close();
         in.close();
